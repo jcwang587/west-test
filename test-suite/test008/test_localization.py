185253060995aa16557b30914a3509f67d63c085
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2026-01-20 10:05:44 -0500

    Refactor code to enhance readability and maintainability

---
+++ test-suite/test008/test_localization.py
@@ -0,0 +1,60 @@
+import numpy as np
+import json
+
+
+def read_localization_from_json(fileName):
+    """
+    Read localization factor from JSON file.
+    """
+
+    with open(fileName, "r") as f:
+        raw_ = json.load(f)
+
+    return np.array(raw_["output"]["L"]["K000001"]["local_factor"], dtype=float)
+
+
+def read_ipr_from_json(fileName):
+    """
+    Read inverse participation ratio (IPR) from JSON file.
+    """
+
+    with open(fileName, "r") as f:
+        raw_ = json.load(f)
+
+    return np.array(raw_["output"]["L"]["K000001"]["ipr"], dtype=float)
+
+
+def test_localization():
+    """
+    Test localization factor.
+    """
+    # get parameters from JSON file
+    with open("./parameters.json", "r") as f:
+        parameters = json.load(f)
+
+    ref_loc = read_localization_from_json("./test008/ref/westpp.json")
+    test_loc = read_localization_from_json("./test008/test.westpp.save/westpp.json")
+
+    np.testing.assert_almost_equal(
+        ref_loc,
+        test_loc,
+        decimal=-np.log10(float(parameters["tolerance"]["westpp"])),
+    )
+
+
+def test_ipr():
+    """
+    Test IPR.
+    """
+    # get parameters from JSON file
+    with open("./parameters.json", "r") as f:
+        parameters = json.load(f)
+
+    ref_ipr = read_ipr_from_json("./test008/ref/westpp.json")
+    test_ipr = read_ipr_from_json("./test008/test.westpp.save/westpp.json")
+
+    np.testing.assert_almost_equal(
+        ref_ipr,
+        test_ipr,
+        decimal=-np.log10(float(parameters["tolerance"]["westpp"])),
+    )
---
+++ test-suite/test008/test_wannier.py
@@ -0,0 +1,60 @@
+import numpy as np
+import json
+
+
+def read_trans_matrix_from_json(fileName):
+    """
+    Read unitary transformation matrix from JSON file.
+    """
+
+    with open(fileName, "r") as f:
+        raw_ = json.load(f)
+
+    return np.array(raw_["output"]["B"]["K000001"]["trans_matrix"], dtype=float)
+
+
+def read_wannier_center_from_json(fileName):
+    """
+    Read Wannier centers from JSON file.
+    """
+
+    with open(fileName, "r") as f:
+        raw_ = json.load(f)
+
+    return np.array(raw_["output"]["B"]["K000001"]["wan_center"], dtype=float)
+
+
+def test_trans_matrix():
+    """
+    Test unitary transformation matrix.
+    """
+    # get parameters from JSON file
+    with open("./parameters.json", "r") as f:
+        parameters = json.load(f)
+
+    ref_trans = read_trans_matrix_from_json("./test008/ref/westpp.json")
+    test_trans = read_trans_matrix_from_json("./test008/test.westpp.save/westpp.json")
+
+    np.testing.assert_almost_equal(
+        np.abs(ref_trans),
+        np.abs(test_trans),
+        decimal=-np.log10(float(parameters["tolerance"]["westpp"])),
+    )
+
+
+def test_wannier_center():
+    """
+    Test Wannier centers.
+    """
+    # get parameters from JSON file
+    with open("./parameters.json", "r") as f:
+        parameters = json.load(f)
+
+    ref_wanc = read_wannier_center_from_json("./test008/ref/westpp.json")
+    test_wanc = read_wannier_center_from_json("./test008/test.westpp.save/westpp.json")
+
+    np.testing.assert_almost_equal(
+        ref_wanc,
+        test_wanc,
+        decimal=-np.log10(float(parameters["tolerance"]["westpp"])),
+    )
